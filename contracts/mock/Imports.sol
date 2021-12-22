3062f85990162682dc3f05066778c3838b5245f2
talentdev2020 <polaris.center@outlook.com>
2021-12-22 02:13:39 -0700

    implement pangolin farming adapter

--- contracts/mock/Imports.sol
+++ contracts/mock/Imports.sol
@@ -3,5 +3,6 @@
 pragma solidity ^0.6.12;
 pragma experimental ABIEncoderV2;
 
-import { IUniswapV2Pair } from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+// import { IUniswapV2Pair } from "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import { IPangolinRouter } from "@pangolindex/exchange-contracts/contracts/pangolin-periphery/interfaces/IPangolinRouter.sol";
 import { IHarvestController } from "@optyfi/defi-legos/ethereum/harvest.finance/contracts/IHarvestController.sol";
